66e8564e89bc0b0e6cad95f884f65e09b237cda8
Paul Camacho <paul@fizzymedia.net>
2013-11-29 12:19:22 -0600

    Cleaned directory and adjusted settings for basic Jekyll integration

--- config.rb
+++ config.rb
@@ -1,10 +1,12 @@
 # Require any additional compass plugins here.
+require "susy"
+require "breakpoint"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "_site/css"
 sass_dir = "_scss"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "javascripts"
 fonts_dir = "fonts"
 
@@ -15,38 +17,3 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 color_output = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
-# Require any additional compass plugins here.
-require "susy"
-require "breakpoint"
-
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
-
-output_style = :nested
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-color_output = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-preferred_syntax = :scss
